d0cacac4ee25e2afffa594437407ca525c7b0279
dscaccianoce <daniele.scaccianoce@accenture.com>
2024-10-09 09:34:47 +0200

    inserimento environments

--- src/app/servizi/rest.service.ts
+++ src/app/servizi/rest.service.ts
@@ -1,22 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestService {
   
-  // private apiUrl = 'http://wedding-app-api.eu-north-1.elasticbeanstalk.com/api'
-  private apiUrl = 'http://localhost:8080/api'
-
+  private apiUrl = environment.BASE_API_URL;
 
   constructor(private httpClient: HttpClient) {}
-
- /*  getData(url: string): Observable<any> {
-    //https://fake-json-api.mock.beeceptor.com/users
-    return this.httpClient.get<any>(url);
-  } */
 
   getApi(url: string): Observable<any> {
     return this.httpClient.get<any>(this.apiUrl + url);
@@ -25,9 +19,5 @@ export class RestService {
   postApi(url: string, data: any) {
     return this.httpClient.post(this.apiUrl + url, data);
   }
-
- /*  getGuestById(url: string, id: number): Observable<any> {
-    return this.httpClient.get<any>(url + "/" + id);
-  } */
   
 }
